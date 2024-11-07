b19db9b9502862d674ebe3153b5353f7bf285fca
Hafsaimranazlan <ik9449080@gmail.com>
2024-11-06 21:26:25 -0800

    project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='i am learning machine learning.',
+    author='imran khan xyz',
+    license='MIT',
+)
